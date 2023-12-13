eaab6a7800e832d859cf2c9facde99c544dea4a6
mathieubx <mathieu.bietrix@gmail.com>
2023-12-13 15:58:46 +0100

    Adding the z vector and a plot

--- Script_test.R
+++ Script_test.R
@@ -1,5 +1,12 @@
 x <- seq(from = 1, to = 10, by = 0.5)
 y <- seq(from = 1, to = 10, by = 0.5)
+z <- c(5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 10, 10, 10, 10, 10, 10, 10, 10, 10)
 
 library(tidyverse)
-tib <- tibble(x, y)
+tib <- tibble(x, y, z)
+
+ggplot(data = tib, 
+       aes(x = y, y = y)) +
+  geom_point(
+    aes(color = z))
+             
